aa931564efcec0681f684bf62afd52c083e30be6
prometheus <berjistechnologies@gmail.com>
2022-07-01 00:37:45 +0300

    toastr

---
+++ app/javascript/external_libraries.js
@@ -0,0 +1,14 @@
+import jquery from 'jquery'
+window.$ = jquery
+
+import * as bootstrap from 'bootstrap'
+window.bootstrap = bootstrap
+
+import * as toastr from 'toastr'
+
+window.jQuery = jquery
+window.$ = jquery
+
+$(function() {
+    console.log("hello world")
+})
